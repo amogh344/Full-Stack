24ab1abd5abbe496f228f7b22bd6314a5f52334b
Amogh <142930584+amogh344@users.noreply.github.com>
2025-02-28 15:43:06 +0000

    edited random2.js

---
+++ CLASS/random2.js
@@ -0,0 +1,13 @@
+let cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix'];
+console.log("The total number of cities ",cities.length);
+
+console.push("France");
+
+console.log("the city added at the end is ",cities[5]);
+
+cities.shift();
+console.log("Cities",cities);
+
+console.log("the index of Los Angeles is ",cities.indexOf("Los Angeles"));
+
+
