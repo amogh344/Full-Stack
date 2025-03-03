78175ceeb32a953f3af18b2aa9391310b92c995c
Amogh <142930584+amogh344@users.noreply.github.com>
2025-03-03 04:11:51 +0000

    random5 and random6 added

--- CLASS/random5.js
+++ CLASS/random5.js
@@ -2,3 +2,9 @@ let a = "ISE";
 let b ="department";
 let c ="AIT";
 console.log(a+" "+b+" "+c);
+
+
+a[0]="h";
+console.log(a[0]);
+
+console.log(a[0]="h");
---
+++ CLASS/random6.js
@@ -0,0 +1,12 @@
+console.log("hello".concat(" world"));
+//javascript is used in web programming
+
+let p=require("prompt-sync")();
+
+let str=p("Enter a line");
+console.log(str.length);
+
+console.log(str.substring(0,10));
+
+
+
