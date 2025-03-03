a18d664d8820e90448bb7b27e604fe77aa4ce9d9
Amogh <142930584+amogh344@users.noreply.github.com>
2025-03-03 03:37:19 +0000

    installed prompt-sync

--- CLASS/random2.js
+++ CLASS/random2.js
@@ -1,18 +1,19 @@
+const prompt = require("prompt-sync")();
+
 let cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix'];
 
 console.log("The total number of cities:", cities.length);
 
-
 cities.push("France");
-
 console.log("The city added at the end is:", cities[cities.length - 1]);
-
 
 cities.shift();
 console.log("Cities after removing the first city:", cities);
 
-
 console.log("The index of 'Los Angeles' is:", cities.indexOf("Los Angeles"));
 
+let newCity = prompt("Enter a city name to add:");
+cities.push(newCity);
 
-
+console.log("The city added at the end is:", cities[cities.length - 1]);
+console.log("Updated cities list:", cities);
