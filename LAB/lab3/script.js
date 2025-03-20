ccfc3aae56e830f1b7a215241f6cfeaafc3cd81e
Amogh <142930584+amogh344@users.noreply.github.com>
2025-03-20 04:45:49 +0000

    lab 4 almost done

--- LAB/lab3/script.js
+++ LAB/lab3/script.js
@@ -1,31 +1,56 @@
-/*Create an object student with properties: name (string), grade (number), subjects (array),
-displayInfo() (method to log the student's details)
-Write a script to dynamically add a passed property to the student object, with a value of true or false
-based on their grade. Create a loop to log all keys and values of the student object.*/
+    /*Create an object student with properties: name (string), grade (number), subjects (array),
+    displayInfo() (method to log the student's details)
+    Write a script to dynamically add a passed property to the student object, with a value of true or false
+    based on their grade. Create a loop to log all keys and values of the student object.*/
 
 const p = require("prompt-sync")();
+let students = [];
 let name;
 let grade;
 let subjects = [];
+let sn = parseInt(p("Enter the number of students to add: "));
 
-function student(name, grade, subjects) {
+function student(name, grade, subjects) 
+{
     this.name = name;
     this.grade = grade;
     this.subjects = subjects;
-};
-
-function enterDetails() {
+}
+    
+function enterDetails() 
+{
     name = p("Enter the name of the student: ");
     grade = parseInt(p("Enter the grade: "));
     subjects = p("Enter the array of subjects: ").split(",");
-};
-
-function displayInfo() {
-    let s = new student(name, grade, subjects);
+    students.push(new student(name, grade, subjects));
+    console.log("\n");
+}
+    
+function checkGrade(grade) 
+{
+    if (grade >= 60)
+        return true;
+    else
+        return false;
+}
+    
+function displayInfo(s) 
+{
     console.log("Name: " + s.name);
     console.log("Grade: " + s.grade);
     console.log("Subjects: " + s.subjects.join(", "));
-};
+    console.log(checkGrade(s.grade) ? "PASS" : "FAIL");
+    console.log("\n");
+}
+    
+for (let i = 0; i < sn; i++) 
+{
+    enterDetails();
+}
+for (let i = 0; i < students.length; i++) 
+{
+    displayInfo(students[i]);
+}
 
-enterDetails();
-displayInfo();
+
+
